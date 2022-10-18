e124f9bee33479e2fc24ea61de89fd661b630e5a
Mark Steve Samson <hello@marksteve.com>
2022-10-18 13:23:58 +0800

    Integrate with supabase until competitor queue

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,172 @@
-import * as React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useParams,
-} from "react-router-dom";
 import { PlayIcon, UserPlusIcon } from "@heroicons/react/24/solid";
-import { apply, tw } from "twind";
 import { Reorder } from "framer-motion";
 import { nanoid } from "nanoid";
+import * as React from "react";
+import {
+  createBrowserRouter,
+  Link,
+  redirect,
+  RouterProvider,
+  useLoaderData,
+  useParams,
+} from "react-router-dom";
+import { apply, tw } from "twind";
+import supabase from "./lib/supabase";
 
-const competitors: Record<number, string> = {
-  1: "Natalia Grossman",
-  2: "Miho Nonaka",
-  3: "Brooke Raboutou",
-  4: "Futabi Ito",
-  5: "Hannah Meul",
-  6: "Oriane Bertone",
-  7: "Stasa Gejo",
-  8: "Jessica Pilz",
-  9: "Camilla Moroni",
-  10: "Franziska Sterrer",
-};
+const router = createBrowserRouter([
+  { path: "/", loader: loadSession, element: <SignIn /> },
+  { path: "/divisions", loader: loadDivisions, element: <Divisions /> },
+  {
+    path: "/divisions/:divisionId/rounds",
+    loader: loadRounds,
+    element: <Rounds />,
+  },
+  {
+    path: "/divisions/:divisionId/rounds/:roundId",
+    loader: loadRound,
+    element: <Round />,
+  },
+]);
 
 export default function App() {
   return (
-    <Router>
-      <div className={tw`w-screen h-screen flex flex-col text-gray-700`}>
-        <header
-          className={tw`bg-purple-500 text-yellow-400 text-3xl text-center leading-relaxed font-black`}
-        >
-          Ale!
-        </header>
-        <Routes>
-          <Route path="/round/:id" element={<Round />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className={tw`w-screen h-screen flex flex-col text-gray-700`}>
+      <header
+        className={tw`bg-purple-500 text-yellow-400 text-3xl text-center leading-relaxed font-black`}
+      >
+        Ale!
+      </header>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
+async function loadSession() {
+  const { data, error } = await supabase.auth.getSession();
+  if (error || !data.session) {
+    if (error) {
+      console.error(error);
+    }
+    return;
+  }
+  return redirect("/divisions");
+}
+
+function SignIn() {
+  useLoaderData();
+
+  const [magicLinkSent, setMagicLinkSent] = React.useState(false);
+
+  async function handleSignIn(e) {
+    e.preventDefault();
+    const email = e.target.elements.email.value;
+    const { error } = await supabase.auth.signInWithOtp({
+      email,
+    });
+    if (error) {
+      console.error(error);
+      return;
+    }
+    setMagicLinkSent(true);
+  }
+
+  return (
+    <form
+      onSubmit={handleSignIn}
+      className={tw`flex-1 flex flex-col gap-5 justify-center items-center`}
+    >
+      <input
+        name="email"
+        type="text"
+        placeholder="Email"
+        disabled={magicLinkSent}
+      />
+      <button
+        type="submit"
+        className={tw([components.button, `bg-pink-500`])}
+        disabled={magicLinkSent}
+      >
+        Sign In
+      </button>
+      {magicLinkSent ? <div>Check your email for a login link!</div> : null}
+    </form>
+  );
+}
+
+async function loadDivisions() {
+  const { data, error } = await supabase.from("divisions").select();
+  if (error) {
+    console.error(error);
+    return;
+  }
+  return data;
+}
+
+function Divisions() {
+  const divisions = useLoaderData();
+  return (
+    <ul className={tw(components.list)}>
+      {divisions.map((division) => (
+        <li key={division.id}>
+          <Link
+            to={`/divisions/${division.id}/rounds`}
+            className={tw(components.item, `h-24`)}
+          >
+            {division.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+async function loadRounds() {
+  const { data, error } = await supabase.from("rounds").select();
+  if (error) {
+    console.error(error);
+    return;
+  }
+  return data;
+}
+
+function Rounds() {
+  const { divisionId } = useParams();
+  const rounds = useLoaderData();
+  return (
+    <ul className={tw(components.list)}>
+      {rounds.map((round) => (
+        <li key={round.id}>
+          <Link
+            to={`/divisions/${divisionId}/rounds/${round.id}`}
+            className={tw(components.item, `h-24`)}
+          >
+            {round.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+async function loadRound({ params }) {
+  const { data: competitors, error } = await supabase
+    .from("competitors")
+    .select()
+    .eq("division_id", params.divisionId);
+  if (error) {
+    console.error(error);
+    return {};
+  }
+  return { competitors };
+}
+
 function Round() {
-  const { id } = useParams();
+  const { competitors } = useLoaderData();
+
+  const competitorsByNumber = competitors.reduce(
+    (prev, curr) => ({ ...prev, [curr.number]: curr }),
+    {}
+  );
   const [queue, setQueue] = React.useState<
     { id: string; number: number; name: string }[]
   >([]);
@@ -50,10 +175,13 @@ function Round() {
     e.preventDefault();
     const input = e.target.elements.number;
     const number = Number(input.value);
-    if (!competitors[number]) {
+    if (!competitorsByNumber[number]) {
       return;
     }
-    setQueue([...queue, { id: nanoid(), number, name: competitors[number] }]);
+    setQueue([
+      ...queue,
+      { id: nanoid(), number, name: competitorsByNumber[number].name },
+    ]);
     input.value = "";
   };
 
@@ -63,13 +191,13 @@ function Round() {
         <Reorder.Group
           values={queue}
           onReorder={setQueue}
-          className={tw`flex-1 flex flex-col p-3 gap-3 overflow-y-auto`}
+          className={tw(components.list)}
         >
           {queue.map((item) => (
             <Reorder.Item
               key={item.id}
               value={item}
-              className={tw`bg-white pl-5 rounded flex justify-between gap-5 border items-center`}
+              className={tw(components.item)}
             >
               <span
                 className={tw`text-gray-500 w-10 text-center border-1 rounded leading-loose`}
@@ -107,6 +235,17 @@ function Round() {
 }
 
 const components = {
+  list: apply`flex-1 flex flex-col p-3 gap-3 overflow-y-auto`,
+  item: apply`
+    bg-white
+    border
+    flex
+    gap-5
+    items-center
+    justify-between
+    pl-5
+    rounded
+  `,
   button: apply`
     border-1
     border-white
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 import { Database } from './database.types'
 
 const supabaseUrl = 'https://mjwhlbnhwjrecyhjdzjj.supabase.co'
-const supabaseKey = import.meta.env.SUPABASE_KEY
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 const supabase = createClient<Database>(supabaseUrl, supabaseKey)
 
 export default supabase
