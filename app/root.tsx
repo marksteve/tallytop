9b289c0d13d671ab11b9b89997fac356c3395595
Mark Steve Samson <hello@marksteve.com>
2022-11-13 22:24:49 +0800

    Add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,23 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node'
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+  ScrollRestoration
+} from '@remix-run/react'
+import styles from '~/styles/app.css'
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }]
+}
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'Tallytop',
+  viewport: 'width=device-width,initial-scale=1',
+})
 
 export default function App() {
   return (
@@ -22,11 +27,13 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <main className="h-screen w-screen flex justify-center items-center">
+          <Outlet />
+        </main>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
