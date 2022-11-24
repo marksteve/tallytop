2e5b3e3091bdcbde54741b620f6ea74f1d3eb973
Mark Steve Samson <hello@marksteve.com>
2022-11-24 08:26:30 +0700

    Implement route QR reading

--- app/loaders.ts
+++ app/loaders.ts
@@ -1,22 +1,28 @@
+import { redirect } from '@remix-run/node'
+import { serverClient } from '~/supabase'
+
+export async function requireSignIn(request) {
+  const supabase = serverClient(request, new Response())
+  const session = await loadSession(supabase)
+  if (!session) {
+    const params = new URLSearchParams()
+    params.set('redirectTo', request.url)
+    return redirect(`/sign-in?${params}`)
+  }
+}
+
 export async function loadSession(supabase) {
   const { data, error } = await supabase.auth.getSession()
-  if (error || !data.session) {
-    if (error) {
-      console.error(error)
-    }
-    return
+  if (error) {
+    throw error
   }
   return data.session
 }
 
 export async function loadUser(supabase) {
   const { data, error } = await supabase.auth.getUser()
-  if (error || !data.user) {
-    if (error) {
-      console.error(error)
-    }
-    return
+  if (error) {
+    throw error
   }
   return data.user
 }
-
---
+++ app/routes/qualis/climb/index.tsx
@@ -0,0 +1,73 @@
+import { getSync as getImageData } from '@andreekeberg/imagedata'
+import {
+  json,
+  redirect,
+  unstable_createMemoryUploadHandler,
+  unstable_parseMultipartFormData,
+  type ActionFunction,
+  type LoaderFunction,
+} from '@remix-run/node'
+import { Form, useLoaderData, useSubmit } from '@remix-run/react'
+import QR from 'jsqr'
+import { requireSignIn } from '~/loaders'
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const redirectParams = await requireSignIn(request)
+  if (redirectParams) {
+    return redirectParams
+  }
+
+  return json({
+    score: 100,
+  })
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const redirectParams = await requireSignIn(request)
+  if (redirectParams) {
+    return redirectParams
+  }
+
+  const uploadHandler = unstable_createMemoryUploadHandler()
+  const formData = await unstable_parseMultipartFormData(request, uploadHandler)
+  const blob = formData.get('capture')
+  const imageData = getImageData(Buffer.from(await blob.arrayBuffer()))
+  const results = await QR(imageData.data, imageData.width, imageData.height)
+  if (!results) {
+    throw new Error('Invalid code')
+  }
+  const { data: id } = results
+  return redirect(`/qualis/climb/${id}`)
+}
+
+export default function ClimbIndex() {
+  const { score } = useLoaderData()
+  const submit = useSubmit()
+
+  const handleCapture = (e) => submit(e.currentTarget)
+
+  return (
+    <div className="flex flex-1 flex-col items-center justify-around">
+      <Form
+        method="post"
+        encType="multipart/form-data"
+        onChange={handleCapture}
+        replace
+      >
+        <label>
+          <div className="button cursor-pointer bg-red text-4xl">CLIMB!</div>
+          <input
+            type="file"
+            name="capture"
+            accept="image/*;capture=camera"
+            className="hidden"
+          />
+        </label>
+      </Form>
+      <div className="flex flex-col gap-5">
+        <div className="text-2xl text-red">Your Points</div>
+        <div className="text-8xl">{score}</div>
+      </div>
+    </div>
+  )
+}
