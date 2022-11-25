aaf6b42df398576e4f8178903b20c6282f2d6c47
Mark Steve Samson <hello@marksteve.com>
2022-11-25 17:06:36 +0700

    Implement self-scoring

--- app/routes/qualis/climb/$id.tsx
+++ app/routes/qualis/climb/$id.tsx
@@ -1,6 +1,11 @@
-import { json, type LoaderFunction } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
-import { requireSignIn } from '~/loaders'
+import {
+  type ActionFunction,
+  json,
+  type LoaderFunction,
+  redirect,
+} from '@remix-run/node'
+import { useLoaderData, useSubmit } from '@remix-run/react'
+import { loadUser, requireSignIn } from '~/loaders'
 import { serverClient } from '~/supabase'
 
 export const loader: LoaderFunction = async ({ request, params }) => {
@@ -15,8 +20,8 @@ export const loader: LoaderFunction = async ({ request, params }) => {
     .select(
       `
       *,
-      attempts(id)
-    `
+      attempts(*)
+      `
     )
     .eq('id', params['id'])
     .single()
@@ -27,15 +32,65 @@ export const loader: LoaderFunction = async ({ request, params }) => {
   return json({ climb })
 }
 
+export const action: ActionFunction = async ({ request, params }) => {
+  const supabase = serverClient(request)
+  const user = await loadUser(supabase)
+  const formData = await request.formData()
+  if (formData.get('clear')) {
+    await supabase
+      .from('attempts')
+      .delete()
+      .eq('competitor_id', user.id)
+      .eq('climb_id', params['id'])
+    return json({ ok: true })
+  }
+  const { error } = await supabase
+    .from('attempts')
+    .upsert({
+      competitor_id: user.id,
+      climb_id: params['id'] ?? '',
+      is_top: true,
+      count: formData.get('isFlash') ? 1 : 100,
+    })
+    .select()
+    .single()
+
+  if (error) {
+    throw error
+  }
+
+  return redirect('/qualis/climb')
+}
+
 export default function Climb() {
   const { climb } = useLoaderData()
+  const submit = useSubmit()
+  const handleTop = () => submit({ isTop: 'true' }, { method: 'post' })
+  const handleFlash = () => submit({ isFlash: 'true' }, { method: 'post' })
+  const handleClear = () => submit({ clear: 'true' }, { method: 'post' })
+  const isTop = climb.attempts[0]?.is_top && climb.attempts[0].count > 1
+  const isFlash = climb.attempts[0]?.is_top && climb.attempts[0].count === 1
+  const activeClass = 'border-4 border-white'
   return (
-    <div className="flex flex-1 flex-col items-center justify-around">
+    <div className="flex flex-1 flex-col items-center justify-around gap-10">
       <div className="climb-number">{climb.name}</div>
       <div className="text-2xl">100 points</div>
-      <div className="flex flex-col gap-20 text-4xl">
-        <button className="button bg-black">Top</button>
-        <button className="button">Flash</button>
+      <div className="flex flex-1 flex-col justify-around text-4xl">
+        <button
+          className={`button bg-black ${isTop ? activeClass : ''}`}
+          onClick={handleTop}
+        >
+          Top
+        </button>
+        <button
+          className={`button ${isFlash ? activeClass : ''}`}
+          onClick={handleFlash}
+        >
+          Flash
+        </button>
+        <button className="button bg-white text-black" onClick={handleClear}>
+          Clear
+        </button>
       </div>
     </div>
   )
