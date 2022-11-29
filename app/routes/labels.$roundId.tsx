c72fea965b576240842b4da631dd75ae86cf0a5f
Mark Steve Samson <hello@marksteve.com>
2022-11-29 17:14:51 +0800

    Update labels layout

--- app/routes/labels.$roundId.tsx
+++ app/routes/labels.$roundId.tsx
@@ -25,44 +25,47 @@ export const loader: LoaderFunction = async ({ request, params }) => {
 export default function Labels() {
   const { climbs } = useLoaderData<LoaderData>()
   return (
-    <div className="flex flex-1 flex-wrap justify-center gap-x-1 gap-y-20 p-10">
+    <>
       {climbs
         .sort((a, b) => ascending(parseInt(a.name, 10), parseInt(b.name, 10)))
-        .map((climb) => (
-          <div
-            key={climb.id}
-            className="flex flex-col justify-center gap-10 border border-black bg-white px-10 py-8 text-black"
-          >
-            <div className="flex flex-col items-center gap-5">
-              <div className="font-cursive leading-[0.5em]">
-                <span
-                  className="text-4xl text-yellow"
-                  style={{
-                    textShadow: '2px 2px 0 black',
-                    WebkitTextStroke: '1px black',
-                  }}
-                >
-                  Queso
-                </span>
-                <br />
-                <span className="pl-7">de Boulder</span>
+        .map((climb, i) => (
+          <>
+            <div
+              key={climb.id}
+              className="inline-flex w-1/2 flex-col items-center justify-center gap-10 border border-black bg-white p-10 text-black"
+            >
+              <div className="flex flex-col items-center gap-5">
+                <div className="font-cursive leading-[0.5em]">
+                  <span
+                    className="text-4xl text-yellow"
+                    style={{
+                      textShadow: '2px 2px 0 black',
+                      WebkitTextStroke: '1px black',
+                    }}
+                  >
+                    Queso
+                  </span>
+                  <br />
+                  <span className="pl-7">de Boulder</span>
+                </div>
+                <div className="climb-number">{climb.name}</div>
               </div>
-              <div className="climb-number">{climb.name}</div>
+              <QRCodeSVG
+                value={climb.id}
+                imageSettings={{
+                  src: '/images/qdb-logo.svg',
+                  width: 32,
+                  height: 32,
+                  excavate: true,
+                }}
+                bgColor="transparent"
+                fgColor="currentColor"
+                size={192}
+              />
             </div>
-            <QRCodeSVG
-              value={climb.id}
-              imageSettings={{
-                src: '/images/qdb-logo.svg',
-                width: 32,
-                height: 32,
-                excavate: true,
-              }}
-              bgColor="transparent"
-              fgColor="currentColor"
-              size={192}
-            />
-          </div>
+            {(i + 1) % 4 === 0 ? <div className="break-after-page" /> : null}
+          </>
         ))}
-    </div>
+    </>
   )
 }
