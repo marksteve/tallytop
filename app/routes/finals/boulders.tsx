46e83f9fa53eda72b268721e8d1283629b9d7627
Mark Steve Samson <hello@marksteve.com>
2022-11-30 15:53:15 +0800

    Require sign in for finals boulders

--- app/routes/finals/boulders.tsx
+++ app/routes/finals/boulders.tsx
@@ -1,13 +1,20 @@
 import { json, type LoaderFunction } from '@remix-run/node'
 import { Link, useLoaderData } from '@remix-run/react'
+import { requireSignIn } from '~/loaders'
 import { serverClient } from '~/supabase'
 
 export const loader: LoaderFunction = async ({ request }) => {
+  const shouldRedirect = await requireSignIn(request)
+  if (shouldRedirect) {
+    return shouldRedirect
+  }
+
   const supabase = serverClient(request)
   const { data: climbs } = await supabase
     .from('climbs')
     .select()
     .eq('round_id', process.env.FINALS_ID)
+
   return json({ climbs })
 }
 
