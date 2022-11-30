56e65f0b7433d23ce19da2cf9b161c2c98b984eb
Mark Steve Samson <hello@marksteve.com>
2022-11-30 13:36:33 +0800

    Add finals boulder page

---
+++ app/routes/finals/boulders.tsx
@@ -0,0 +1,32 @@
+import { json, type LoaderFunction } from '@remix-run/node'
+import { Link, useLoaderData } from '@remix-run/react'
+import { serverClient } from '~/supabase'
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const supabase = serverClient(request)
+  const { data: climbs } = await supabase
+    .from('climbs')
+    .select()
+    .eq('round_id', process.env.FINALS_ID)
+  return json({ climbs })
+}
+
+export default function Boulders() {
+  const { climbs } = useLoaderData()
+  return (
+    <div className="flex flex-1 flex-col items-center gap-10 p-10">
+      <h2 className="text-6xl">Finals Boulders</h2>
+      <div className="flex max-w-screen-sm flex-wrap items-center justify-center gap-5 text-4xl">
+        {climbs.map((climb) => (
+          <Link
+            key={climb.id}
+            to={`/finals/boulders/${climb.id}`}
+            className="button w-1/4 text-center"
+          >
+            {climb.name}
+          </Link>
+        ))}
+      </div>{' '}
+    </div>
+  )
+}
