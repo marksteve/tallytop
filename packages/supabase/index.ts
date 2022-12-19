438569dc62480a71ee9d6be689c30b57f9149174
Mark Steve Samson <hello@marksteve.com>
2022-12-19 08:09:49 +0800

    Init supabase package

---
+++ packages/supabase/index.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export default createClient(
+  'https://mjwhlbnhwjrecyhjdzjj.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qd2hsYm5od2pyZWN5aGpkempqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYwNTc3NTgsImV4cCI6MTk4MTYzMzc1OH0.4w8bxoyaWBcnP4aOzgi4PrEVX2Ll4GBaKQ7tjvV3y4U'
+)
