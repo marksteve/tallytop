a7e46c2606d49e4e53e5711cf35c05eebf1c4327
Mark Steve Samson <hello@marksteve.com>
2022-12-19 12:15:33 +0800

    Add Launch Week 6 link

--- apps/timer/src/routes/+layout.svelte
+++ apps/timer/src/routes/+layout.svelte
@@ -7,3 +7,12 @@
 </svelte:head>
 
 <slot />
+
+<a
+  href="https://www.madewithsupabase.com/launch-week-6"
+  target="_blank"
+  rel="noreferrer"
+  class="absolute bottom-0 right-0 p-5 indent-8"
+>
+  Made with <img src="/images/supabase-logo-wordmark--light.svg" alt="Supabase" width="128" />
+</a>
