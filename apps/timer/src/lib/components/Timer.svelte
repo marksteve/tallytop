c6eb3b59ded652d2a2fb46c46107a295d44ef2c6
Mark Steve Samson <hello@marksteve.com>
2022-12-19 10:43:51 +0800

    Sync time with supabase

--- apps/timer/src/lib/components/Timer.svelte
+++ apps/timer/src/lib/components/Timer.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
   import { browser } from '$app/environment'
   import { goto } from '$app/navigation'
+  import supabase from '@tallytop/supabase'
   import { Button } from '@tallytop/ui'
   import { Howl } from 'howler'
   import { nanoid } from 'nanoid'
-  import parseMs from 'parse-ms'
+  import parseMs, { type TimeComponents } from 'parse-ms'
   import ArrowCounterClockwise from 'phosphor-svelte/lib/ArrowCounterClockwise'
   import Eye from 'phosphor-svelte/lib/eye'
   import Play from 'phosphor-svelte/lib/play'
   import Stop from 'phosphor-svelte/lib/stop'
+  import { onDestroy, onMount } from 'svelte'
+
+  const ID_LENGTH = 6
+
+  type Status = 'started' | 'running' | 'stopped'
+  type Event = 'start' | 'stop' | 'reset'
 
   let endTime: number
   let elapsed = 0
-  let status = 'stopped'
+  let status: Status = 'stopped'
 
   export let duration = 5 * 60 * 1000
   let time = parseMs(duration)
@@ -26,21 +33,70 @@
 
   export let viewMode = false
   export let id: string = browser
-    ? window.localStorage.getItem('tallytop-timer-id') ?? nanoid()
-    : nanoid()
+    ? window.localStorage.getItem('tallytop-timer-id') ?? nanoid(ID_LENGTH)
+    : nanoid(ID_LENGTH)
   if (browser && !viewMode) {
     window.localStorage.setItem('tallytop-timer-id', id)
   }
 
-  function start() {
-    endTime = Date.now() + duration - elapsed
+  const channel = supabase.channel(
+    `tallytop:${id}`,
+    viewMode
+      ? undefined
+      : {
+          config: { presence: { key: id } }
+        }
+  )
+  onMount(() => {
+    if (!browser) {
+      return
+    }
+    if (viewMode) {
+      channel.on('broadcast', { event: '*' }, ({ event, payload }) => {
+        switch (event as Event) {
+          case 'start':
+            start(payload.endTimeInit)
+            break
+          case 'stop':
+            stop()
+            break
+          case 'reset':
+            reset()
+            break
+        }
+      })
+      channel.on('presence', { event: 'sync' }, () => {
+        const prescence = channel.presenceState()?.[id]
+        if (!prescence) {
+          return
+        }
+        time = prescence[0] as unknown as TimeComponents
+      })
+    }
+    channel.subscribe()
+  })
+  onDestroy(() => {
+    if (!browser) {
+      return
+    }
+    Promise.all([channel.unsubscribe(), channel.untrack()])
+  })
+
+  function start(endTimeInit?: number) {
+    endTime = endTimeInit ? endTimeInit : Date.now() + duration - elapsed
     status = 'started'
+    if (!viewMode) {
+      channel.send({ type: 'broadcast', event: 'start', payload: { endTimeInit: endTime } })
+    }
   }
 
   function stop() {
     const remaining = endTime - Date.now()
     elapsed = duration - remaining
     status = 'stopped'
+    if (!viewMode) {
+      channel.send({ type: 'broadcast', event: 'stop' })
+    }
   }
 
   function reset() {
@@ -48,11 +104,15 @@
     elapsed = 0
     status = 'stopped'
     playedBeeps = []
+    if (!viewMode) {
+      channel.send({ type: 'broadcast', event: 'reset' })
+    }
   }
 
   function tick() {
     if (status === 'running') {
       let remaining = endTime - Date.now()
+      time = parseMs(remaining)
 
       if (remaining < 0) {
         remaining = 0
@@ -62,10 +122,7 @@
         sound.end.play()
       }
 
-      time = parseMs(remaining)
-
       const seconds = Math.ceil(remaining / 1000)
-
       if (
         !playedBeeps.includes(seconds) &&
         (seconds === Math.ceil(duration / 1000) || beeps.includes(seconds))
@@ -74,7 +131,20 @@
         playedBeeps = [...playedBeeps, seconds]
       }
 
+      if (!viewMode) {
+        trackTime()
+      }
+
       requestAnimationFrame(tick)
+    }
+  }
+
+  let broadcastLimit = 1 // rps
+  let lastBroadcast: number = 0
+  function trackTime() {
+    if (Date.now() - lastBroadcast > broadcastLimit * 1000) {
+      channel.track(time)
+      lastBroadcast = Date.now()
     }
   }
 
@@ -103,11 +173,11 @@
 {#if !viewMode}
   <div class="flex gap-10 text-4xl">
     {#if status === 'stopped'}
-      <Button class="bg-emerald-200 px-10 py-5" on:click={start}><Play /></Button>
+      <Button class="bg-emerald-200 px-10 py-5" on:click={() => start()}><Play /></Button>
     {:else}
-      <Button class="bg-orange-200 px-10 py-5" on:click={stop}><Stop /></Button>
+      <Button class="bg-orange-200 px-10 py-5" on:click={() => stop()}><Stop /></Button>
     {/if}
-    <Button class="px-10 py-5" on:click={reset}><ArrowCounterClockwise /></Button>
-    <Button class="px-10 py-5" on:click={view}><Eye /></Button>
+    <Button class="px-10 py-5" on:click={() => reset()}><ArrowCounterClockwise /></Button>
+    <Button class="px-10 py-5" on:click={() => view()}><Eye /></Button>
   </div>
 {/if}
