e621206ced248b63795987e314d7d34bb59f8ab7
Mark Steve Samson <hello@marksteve.com>
2023-02-03 17:25:10 +0800

    Make landing look good

--- apps/2023-02-roc-royale/src/routes/+page.svelte
+++ apps/2023-02-roc-royale/src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+  import { Logo } from '@tallytop/ui'
   let links = [
-    ['JUDGE', '/judge', 'rotate-3'],
     ['SEE SCORES', '/scores', '-rotate-2'],
+    ['JUDGE', '/judge', 'rotate-3'],
     ['Sponsors', '/sponsors', 'rotate-1']
   ]
 </script>
@@ -9,21 +10,26 @@
 <div class="bg-roc-hotpink">
   <img src="/images/bg.jpg" class="fixed h-full w-full object-cover" />
 
-  <div class="flex min-h-[80vh] items-center justify-center">
-    <img src="/images/roc-royale-logo.png" class="relative" />
-    <img src="/images/ccm-logo.png" class="relative" />
+  <div class="flex items-center p-10 md:max-h-[60vh] md:justify-center">
+    <img src="/images/roc-royale-logo.png" class="relative flex-1 md:flex-none" />
+    <img src="/images/ccm-logo.png" class="relative flex-1 md:flex-none" />
   </div>
 
-  <div class="item-stretch relative flex h-[20vh] gap-10 p-10">
+  <div class="relative flex flex-col gap-10 p-10 md:flex-row">
     {#each links as [link, href, _class]}
       <div class="flex flex-1">
         <a
           {href}
-          class="bg-roc-black font-title hover:bg-roc-yellow hover:text-roc-black flex flex-1 items-center justify-center text-8xl text-white {_class}"
+          class="bg-roc-black font-title hover:bg-roc-yellow hover:text-roc-black flex flex-1 items-center justify-center text-8xl text-white {_class} -indent-5 leading-[0.8em]"
         >
           {link}
         </a>
       </div>
     {/each}
+  </div>
+
+  <div class="relative flex items-center justify-center gap-2 whitespace-nowrap p-10 text-2xl">
+    Powered by
+    <Logo class="h-14" />
   </div>
 </div>
--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './components/Button.svelte'
+export { default as Logo } from './components/Logo.svelte'
 export { default as Timer } from './components/Timer.svelte'
