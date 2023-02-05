0f9e1c6e817060a78ca1c9a626b78fd108818ec1
Mark Steve Samson <hello@marksteve.com>
2023-02-05 11:00:22 +0800

    Create store for timer queue

---
+++ apps/timer/src/lib/stores.ts
@@ -0,0 +1,3 @@
+import { writable } from 'svelte/store'
+
+export const timerQueue = writable([[5 * 60 * 1000, '5-min timer'] as const])
--- apps/timer/src/routes/+page.svelte
+++ apps/timer/src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script>
   import { browser } from '$app/environment'
   import { Logo, Timer } from '@tallytop/ui'
+  import { timerQueue } from '$lib/stores'
+
+  const [duration, description] = $timerQueue[0]
 </script>
 
 <div class="flex min-h-screen flex-1 flex-col items-center justify-center gap-10">
   <Logo width="128" />
-  <Timer {browser} />
+  <Timer {duration} {browser} />
+  <div class="text-2xl">{description}</div>
 </div>
