c9a641f1f0bbf39dccefafb11a814196a1a106fd
Mark Steve Samson <hello@marksteve.com>
2023-02-18 09:42:47 +0800

    Add sponsors page

---
+++ apps/2023-02-roc-royale/src/routes/(app)/sponsors/+page.svelte
@@ -0,0 +1,22 @@
+<div
+  class="font-title flex w-full flex-col items-center gap-20 p-5 text-center md:flex-row md:p-20"
+>
+  <div class="flex flex-1 flex-col items-center gap-20">
+    <div class="text-4xl md:text-6xl">Roc Royale 2023 is co-presented by</div>
+    <img src="/images/sponsors/kinetiqq.png" alt="Kinetiqq" />
+  </div>
+  <div class="flex flex-1 flex-col items-center gap-20">
+    <div class="text-3xl">We'd like to thank our very generous sponsors</div>
+    <div class="flex items-center justify-center gap-5 md:gap-10">
+      <div class="w-1/2"><img src="/images/sponsors/wheyl.png" alt="Wheyl" /></div>
+      <div class="w-1/2"><img src="/images/sponsors/tnf.png" alt="The North Face" /></div>
+    </div>
+    <div class="flex items-center justify-center gap-5 md:gap-10">
+      <div class="w-1/6"><img src="/images/sponsors/lyfe.png" alt="Lyfe" /></div>
+      <div class="w-1/6"><img src="/images/sponsors/1c.jpeg" alt="1C" /></div>
+      <div class="w-1/6"><img src="/images/sponsors/boundless.png" alt="Boundless" /></div>
+      <div class="w-1/6"><img src="/images/sponsors/clutch.png" alt="Clutch" /></div>
+      <div class="w-1/6"><img src="/images/sponsors/red-bull.png" alt="Red Bull" /></div>
+    </div>
+  </div>
+</div>
---
+++ apps/2023-02-roc-royale/src/routes/(app)/sponsors/+page.ts
@@ -0,0 +1,7 @@
+import type { PageLoad } from './$types'
+
+export const load = (() => {
+  return {
+    title: ['Sponsors', 'rotate-1']
+  }
+}) satisfies PageLoad
