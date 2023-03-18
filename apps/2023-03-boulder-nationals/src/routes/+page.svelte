8bfa0d2f06dcada9b2ba55e500ce7ddda9e4bfcd
Mark Steve Samson <hello@marksteve.com>
2023-03-18 13:23:02 +0800

    Add sponsor logos

--- apps/2023-03-boulder-nationals/src/routes/+page.svelte
+++ apps/2023-03-boulder-nationals/src/routes/+page.svelte
@@ -1,3 +1,7 @@
-<div class="absolute inset-0 flex items-center justify-center p-10">
+<div class="flex min-h-screen flex-col items-center justify-center p-10">
   <img src="/images/logo.svg" />
+  <div class="flex flex-col gap-10 md:flex-row items-center">
+    <img class="max-w-xs" src="/images/sponsors/pilipinas-climbing.png" />
+    <img class="max-h-80 max-w-xs" src="/images/sponsors/bhive.png" />
+  </div>
 </div>
