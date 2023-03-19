f94585f363680e11ecb14c581955b8254af16b45
Mark Steve Samson <hello@marksteve.com>
2023-03-19 08:44:56 +0800

    Style header

--- apps/2023-03-boulder-nationals/src/routes/(app)/+layout.svelte
+++ apps/2023-03-boulder-nationals/src/routes/(app)/+layout.svelte
@@ -10,11 +10,16 @@
   <button on:click={logout}>Logout</button>
 {/if}
 
-<div class="text-brand flex items-center justify-between p-5">
+<div class="text-brand flex justify-between p-5">
   <a class="w-32" href="/">
     <img src="/images/logo.svg" class="w-full" />
   </a>
-  <h2 class="text-3xl font-bold">{$page.data.title}</h2>
+  <div class="relative flex items-center">
+    <img class="absolute h-full" src="/images/header-bg.svg" />
+    <h2 class="relative bg-white text-3xl font-bold">
+      {$page.data.title}
+    </h2>
+  </div>
 </div>
 
 <slot />
