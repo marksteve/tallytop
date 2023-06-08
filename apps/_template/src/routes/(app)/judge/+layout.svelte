12fad8c55265600f0a4d3fe8e37a1929c4695a60
Mark Steve Samson <hello@marksteve.com>
2023-06-08 15:37:32 +0800

    Remove _template

--- apps/_template/src/routes/(app)/judge/+layout.svelte
+++
@@ -1,3 +0,0 @@
-<div class="font-title flex flex-1 flex-col items-center justify-center gap-10 text-6xl">
-  <slot />
-</div>
--- apps/_template/src/routes/(app)/judge/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import type { PageData } from './$types'
-
-  export let data: PageData
-</script>
-
-{#each data.rounds as [path, name]}
-  <a href="/judge/{path}">{name}</a>
-{/each}
