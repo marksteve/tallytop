7df078e976eed56abc3a501b69aafab0c972cde6
Mark Steve Samson <hello@marksteve.com>
2023-07-25 10:25:28 +0800

    Preload timer icons

--- apps/timer/src/routes/+layout.svelte
+++ apps/timer/src/routes/+layout.svelte
@@ -4,6 +4,9 @@
 
 <svelte:head>
   <title>Tallytop Timer</title>
+  <link rel="preload" as="image" href="/images/start.png" />
+  <link rel="preload" as="image" href="/images/stop.png" />
+  <link rel="preload" as="image" href="/images/reset.png" />
 </svelte:head>
 
 <slot />
