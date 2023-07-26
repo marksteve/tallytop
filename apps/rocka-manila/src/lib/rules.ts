1abe8e34ee67f7bf7a4f8e5865963ead02971d74
Mark Steve Samson <hello@marksteve.com>
2023-07-26 15:21:09 +0800

    Prevent negative points

--- apps/rocka-manila/src/lib/rules.ts
+++ apps/rocka-manila/src/lib/rules.ts
@@ -1,6 +1,9 @@
 export const qualisScore = (tally: any) => {
   const flash = tally.attempts === 1
-  return tally.top
-    ? parseFloat(tally.problem) + (flash ? 2 : 0) - (flash ? 0 : tally.attempts - 1) * 0.2
-    : 0
+  return Math.max(
+    0,
+    tally.top
+      ? parseFloat(tally.problem) + (flash ? 2 : 0) - (flash ? 0 : tally.attempts - 1) * 0.2
+      : 0
+  )
 }
