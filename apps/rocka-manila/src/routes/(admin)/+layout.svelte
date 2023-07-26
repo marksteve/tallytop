9f63f222c0b0c18a8fcca61bffdbf4551fed378d
Mark Steve Samson <hello@marksteve.com>
2023-07-26 14:14:09 +0800

    Add header link

--- apps/rocka-manila/src/routes/(admin)/+layout.svelte
+++ apps/rocka-manila/src/routes/(admin)/+layout.svelte
@@ -2,6 +2,8 @@
   import 'carbon-components-svelte/css/white.css'
 </script>
 
-<h1 class="text-2xl font-bold px-8 border-b border-solid leading-loose">Tallytop</h1>
+<h1 class="text-2xl font-bold px-8 border-b border-solid leading-loose">
+  <a href="/admin" class="text-current">Tallytop</a>
+</h1>
 
 <slot />
