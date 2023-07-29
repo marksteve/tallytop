6ce24d9f57e890e983c890972249dae376028176
Mark Steve Samson <hello@marksteve.com>
2023-07-29 17:31:33 +0800

    Hide qualis results

--- apps/rocka-manila/src/routes/+page.svelte
+++ apps/rocka-manila/src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <div class="flex flex-col bg-rockamanila-bg min-h-screen justify-center items-center p-10">
   <div class="max-w-screen-sm flex flex-col gap-10">
     <img src="/images/rocka-manila-logo.png" alt="Rocka Manila" />
-    <a href="/novice_men/results/qualis" class="active:scale-95"
-      ><img src="/images/qualis.svg" alt="Qualis" /></a
-    >
     <a href="/novice_men/results/finals" class="active:scale-95"
       ><img src="/images/finals.svg" alt="Finals" /></a
     >
