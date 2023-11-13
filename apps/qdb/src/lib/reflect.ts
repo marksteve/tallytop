c35d47f679d02232d590beb7334842537f76ab29
Mark Steve Samson <hello@marksteve.com>
2023-11-13 23:58:05 +0800

    Publish reflect

--- apps/qdb/src/lib/reflect.ts
+++ apps/qdb/src/lib/reflect.ts
@@ -1,11 +1,11 @@
+import { PUBLIC_REFLECT_URL } from '$env/static/public'
 import { Reflect } from '@rocicorp/reflect/client'
 import { mutators } from '../reflect/mutators'
 
 export const r = new Reflect({
   userID: 'public',
   roomID: 'qdb-2023',
-  server: 'http://localhost:8080',
+  server: PUBLIC_REFLECT_URL,
   mutators,
   kvStore: 'idb',
-
 })
