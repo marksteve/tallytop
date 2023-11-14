2c86edc7630ef816497e937abea4a49f02ce181f
Mark Steve Samson <hello@marksteve.com>
2023-11-14 21:33:51 +0800

    Pick problem

---
+++ apps/qdb/src/routes/judge/teams/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import Button from '$lib/components/button.svelte'
+  import { Splide, SplideSlide } from '@splidejs/svelte-splide'
+  import type { SplideEvents } from '@splidejs/svelte-splide/components/Splide/Splide.svelte'
+  import '@splidejs/svelte-splide/css'
+
+  const problems = [...Array(6).keys()].map((i) => i + 1)
+
+  let selectedProblem = 1
+
+  const selectProblem = (e: SplideEvents['active']) => {
+    selectedProblem = problems[e.detail.Slide.index]
+  }
+</script>
+
+<div class="flex flex-col items-center gap-5 p-5">
+  <img src="/images/logo.png" alt="Logo" class="w-24" />
+  <h1 class="text-brand-red font-serif text-6xl">Judge Teams</h1>
+  <Splide
+    class="w-full"
+    options={{
+      type: 'loop',
+      arrows: false,
+      pagination: false,
+      gap: 20,
+      heightRatio: 1,
+    }}
+    on:active={selectProblem}
+  >
+    {#each problems as problem}
+      <SplideSlide>
+        <div
+          class="text-brand-red flex h-full flex-col items-center justify-center rounded-tl-full rounded-tr-full border border-current"
+        >
+          <div class="text-3xl">Problem</div>
+          <div class="text-6xl">{problem}</div>
+        </div>
+      </SplideSlide>
+    {/each}
+  </Splide>
+  <Button class="text-3xl">
+    <a href={`/judge/teams/${selectedProblem}`}>Judge</a>
+  </Button>
+</div>
