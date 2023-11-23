43121ee2ad10ada79fc27233ed66ce4621206654
Mark Steve Samson <hello@marksteve.com>
2023-11-23 08:25:39 +0800

    Fix public layout

--- apps/qdb/src/routes/(public)/+layout.svelte
+++ apps/qdb/src/routes/(public)/+layout.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-  import { goto } from '$app/navigation'
   import '../../app.css'
-
-  const handleSelectCategory = (e) => {
-    const category = e.target.value
-    goto(`/manage/${category}`)
-  }
 </script>
 
 <svelte:head>
@@ -21,19 +15,4 @@
   <link rel="preload" as="image" href="/images/whole.png" />
 </svelte:head>
 
-<div class="grid grid-cols-3 items-start gap-5 p-5">
-  <h1 class="text-brand-red col-span-3 font-serif text-6xl">
-    <img
-      src="/images/logo.png"
-      alt="Logo"
-      class="inline-block w-24 align-middle"
-    />
-    Manage
-    <select class="bg-transparent font-serif" on:change={handleSelectCategory}>
-      <option value="open-m">Open Men's</option>
-      <option value="open-w">Open Women's</option>
-      <option value="teams">Teams</option>
-    </select>
-  </h1>
-  <slot />
-</div>
+<slot />
---
+++ apps/qdb/src/routes/(public)/manage/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { goto } from '$app/navigation'
+
+  const handleSelectCategory = (e) => {
+    const category = e.target.value
+    goto(`/manage/${category}`)
+  }
+</script>
+
+<div class="grid grid-cols-3 items-start gap-5 p-5">
+  <h1 class="text-brand-red col-span-3 font-serif text-6xl">
+    <img
+      src="/images/logo.png"
+      alt="Logo"
+      class="inline-block w-24 align-middle"
+    />
+    Manage
+    <select class="bg-transparent font-serif" on:change={handleSelectCategory}>
+      <option value="open-m">Open Men's</option>
+      <option value="open-w">Open Women's</option>
+      <option value="teams">Teams</option>
+    </select>
+  </h1>
+  <slot />
+</div>
