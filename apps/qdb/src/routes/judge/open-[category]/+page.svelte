6746ff684cfae4483b70b9512469b4aad1be8cdb
Mark Steve Samson <hello@marksteve.com>
2023-11-23 13:05:50 +0800

    Fix rounds picker

--- apps/qdb/src/routes/judge/open-[category]/+page.svelte
+++ apps/qdb/src/routes/judge/open-[category]/+page.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
+  import { page } from '$app/stores'
   import { variants } from '@tallytop/ui'
   variants
 </script>
 
 <div class={variants.list()}>
   <a
-    href="./open-m/qualis"
+    href={`./open-${$page.params.category}/qualis`}
     class={variants.listItem({ class: 'font-tanker text-3xl' })}
   >
     qualis
   </a>
   <a
-    href="./open-m/finals"
+    href={`./open-${$page.params.category}/qualis`}
     class={variants.listItem({ class: 'font-tanker text-3xl' })}
   >
     finals
